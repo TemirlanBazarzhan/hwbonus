d0ccd785ba58b1a56519513c2e5a08a5a1ac040d
TemirlanBazarzhan <72912147+TemirlanBazarzhan@users.noreply.github.com>
2021-12-05 03:36:46 +0600

    Update config.py

--- crudwebapp/instance/config.py
+++ crudwebapp/instance/config.py
@@ -1,4 +1,5 @@
 # instance/config.py
 
 SECRET_KEY = '<^>YOUR_SECRET_KEY^>'
-SQLALCHEMY_DATABASE_URI = 'postgresql://localhost:5432/service'
+SQLALCHEMY_DATABASE_URI = 'postgres://wxbzmoblxqgvrz:5c13d4cbd00ec971f22e4139ec4021e2326ccd0bce7bac0a991ac139277f582a@ec2-34-254-120-2.eu-west-1.compute.amazonaws.com:5432/d7rkebec758t8
+'
